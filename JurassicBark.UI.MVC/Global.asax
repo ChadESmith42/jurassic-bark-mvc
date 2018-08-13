edaa17aa5ca6e99ba120680bfcb3ff0bfcd85cc2
Chad Smith <chad.e.smith@outlook.com>
2018-08-13 09:46:57 -0500

    INITIAL COMMIT

---
+++ JurassicBark.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JurassicBark.UI.MVC.MvcApplication" Language="C#" %>
---
+++ JurassicBark.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JurassicBark.UI.MVC.Startup))]
+namespace JurassicBark.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
