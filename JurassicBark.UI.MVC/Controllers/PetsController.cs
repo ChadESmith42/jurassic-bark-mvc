35a5c4ecf340c968d30a2334078e4c4fc1368d0d
Chad Smith <chad.e.smith@outlook.com>
2018-08-29 16:26:30 -0500

    COMMIT: Changes to how PetPhotos are handled on the server. The Pet Controller Delete ActionResult now deletes the PetPhoto from the server. The Pet Controller Edit ActionResult now deletes the previous version of the PetPhoto. This is intended to keep the server data storage to a minimum. Edited PetPhotos are concatenated with the Pet.Name and Pet.PetID, as well as the date of creation.

--- JurassicBark.UI.MVC/Controllers/PetsController.cs
+++ JurassicBark.UI.MVC/Controllers/PetsController.cs
@@ -85,6 +85,7 @@ namespace JurassicBark.UI.MVC.Controllers
             {
                 pet.OwnerID = User.Identity.GetUserId();
                 pet.IsActive = true;
+                pet.DateAdded = DateTime.Now;
             }
 
             if (User.IsInRole("Admin"))
@@ -102,12 +103,16 @@ namespace JurassicBark.UI.MVC.Controllers
                     string imgExt = Path.GetExtension(PetPhoto.FileName).ToLower();
                     //Create string array of allowed file extensions
                     string[] allowedExtensions = { ".png", ".jpg", ".jpeg", ".gif" };
+                    //Create identifier to distinguish pet image:
+                    //Select the "largest" PetID and then increment that value by 1.
+                    int maxPetID = uow.PetRepository.Get().Last().PetID;
+                    maxPetID = maxPetID + 1;
                     //Check image is allowed
                     if (allowedExtensions.Contains(imgExt))
                     {
                         //Save file name with identifier
                         imageName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "_" +
-                            Path.GetFileName(PetPhoto.FileName);
+                            pet.Name + "_" + maxPetID + imgExt;
 
                         //Set the path on the server to store the image:
                         string savePath = Server.MapPath("~/Content/images/pets/");
@@ -166,11 +171,16 @@ namespace JurassicBark.UI.MVC.Controllers
         [Authorize]
         public ActionResult Edit([Bind(Include = "PetID,Name,OwnerID,PetPhoto,SpecialNotes,IsActive,DateAdded")] Pet pet, HttpPostedFileBase fileUpload)
         {
+            //Creates copy of pet from DB. Will use the PetPhoto name to delete the old phot from
+            //the server. 
+            Pet deletePet = uow.PetRepository.Get().Where(p => p.PetID == pet.PetID).First();
+            //Variable to trigger deleting old photo. If no new photo is uploaded, this remains false
+            bool deleteOldPhoto = false;
+
             if (!User.IsInRole("Admin"))
             {
                 pet.OwnerID = User.Identity.GetUserId();
             }
-
 
             if (User.IsInRole("Admin"))
             {
@@ -181,7 +191,8 @@ namespace JurassicBark.UI.MVC.Controllers
             {
                 if (fileUpload != null)
                 {
-                    pet.PetPhoto = fileUpload.FileName;
+                    //Sets condition to delete old photo
+                    deleteOldPhoto = true;   
                     //Empty string to use if image needs to be renamed.
                     string imageName = "";
                     //Create variable to capture the file extension
@@ -193,7 +204,7 @@ namespace JurassicBark.UI.MVC.Controllers
                     {
                         //Save file name with identifier
                         imageName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "_" +
-                            Path.GetFileName(fileUpload.FileName);
+                            pet.Name + "_" + pet.PetID + imgExt;
 
                         //Set the path on the server to store the image:
                         string savePath = Server.MapPath("~/Content/images/pets/");
@@ -201,7 +212,10 @@ namespace JurassicBark.UI.MVC.Controllers
                         Image petImage = Image.FromStream(fileUpload.InputStream);
                         FileUtilities.ResizeImage(savePath, imageName, petImage, 500, 100);
                         pet.PetPhoto = imageName;
+
                     }
+
+                    //Sets the name of the PetPhoto based on the existing DB values.
                     else
                     {
                         pet.PetPhoto = pet.PetPhoto;
@@ -212,6 +226,65 @@ namespace JurassicBark.UI.MVC.Controllers
 
                 db.Entry(pet).State = EntityState.Modified;
                 db.SaveChanges();
+
+                //If a new photo was uploaded, delete the old photo
+                if (deleteOldPhoto)
+                {
+                    //Set the server path for the PetPhoto image
+                    string path = Server.MapPath("~/Content/images/pets/");
+
+                    //Concatenate the pat and image name to a string
+                    string photoPath = path + deletePet.PetPhoto;
+                    string thumbPhotoPath = path + "t_" + deletePet.PetPhoto;
+                    //Set a number of times to try to delete the file (in case the server thinks the
+                    //file is in use.
+                    int RetriesOnError = 3;
+
+                    //Create a delay for the delete retry
+                    int DelayOnRetry = 1000;
+
+                    //Process to delete the existing image from the server
+                    //The for loop is necessary for the process to try 3 times to delete the photo
+                    //Catch will delay for 1 second before trying again.
+                    for (int i = 0; i < RetriesOnError; ++i)
+                    {
+                        try
+                        {
+                            //Create a file variable to hold the photo location
+                            var file = new FileInfo(photoPath);
+                            //Delete the file ...
+                            file.Delete();
+                        }
+                        catch (IOException)
+                        {
+                            System.Threading.Thread.Sleep(DelayOnRetry);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            System.Threading.Thread.Sleep(DelayOnRetry);
+                        }
+                    }
+                    //Delete thumbnail file also:
+                    for (int i = 0; i < RetriesOnError; ++i)
+                    {
+                        try
+                        {
+                            //Create a file variable to hold the photo location
+                            var file = new FileInfo(thumbPhotoPath);
+                            //Delete the file ...
+                            file.Delete();
+                        }
+                        catch (IOException)
+                        {
+                            System.Threading.Thread.Sleep(DelayOnRetry);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            System.Threading.Thread.Sleep(DelayOnRetry);
+                        }
+                    }
+                }
+                
                 return RedirectToAction("Index");
             }
             return View(pet);
@@ -249,12 +322,77 @@ namespace JurassicBark.UI.MVC.Controllers
             //Check if user is Admin and remove record:
             if (User.IsInRole("Admin"))
             {
+                //Create a list of existing Reservation objects
                 List<Reservation> reservationList = new List<Reservation>();
+                //Select only the Reservation objects belonging to pet
                 reservationList = uow.ReservationRepository.Get().Where(r => r.PetID == id).ToList();
+                //Delete all Reservation objects for pet
                 foreach (Reservation item in reservationList)
                 {
                     uow.ReservationRepository.Remove(item);
                 }
+
+
+                #region Remove PetPhoto File
+                /*In order to save on server space, delete the PetPhoto image from the server
+                when deleting the Pet object. Since the photo isn't stored in the database
+                this region will attempt to delete the file when the pet is deleted.*/
+
+                //Set the server path for the PetPhoto image
+                string path = Server.MapPath("~/Content/images/pets/");
+
+                //Concatenate the pat and image name to a string
+                string photoPath = path + pet.PetPhoto;
+                string thumbPhotoPath = path + "t_" + pet.PetPhoto;
+                //Set a number of times to try to delete the file (in case the server thinks the
+                //file is in use.
+                int RetriesOnError = 3;
+
+                //Create a delay for the delete retry
+                int DelayOnRetry = 1000;
+
+                //Process to delete the existing image from the server
+                //The for loop is necessary for the process to try 3 times to delete the photo
+                //Catch will delay for 1 second before trying again.
+                for (int i = 0; i < RetriesOnError; ++i)
+                {
+                    try
+                    {
+                        //Create a file variable to hold the photo location
+                        var file = new FileInfo(photoPath);
+                        //Delete the file ...
+                        file.Delete();
+                    }
+                    catch (IOException)
+                    {
+                        System.Threading.Thread.Sleep(DelayOnRetry);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        System.Threading.Thread.Sleep(DelayOnRetry);
+                    }
+                }
+                //Delete thumbnail file also:
+                for (int i = 0; i < RetriesOnError; ++i)
+                {
+                    try
+                    {
+                        //Create a file variable to hold the photo location
+                        var file = new FileInfo(thumbPhotoPath);
+                        //Delete the file ...
+                        file.Delete();
+                    }
+                    catch (IOException)
+                    {
+                        System.Threading.Thread.Sleep(DelayOnRetry);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        System.Threading.Thread.Sleep(DelayOnRetry);
+                    }
+                }
+                #endregion
+
                 uow.PetRepository.Remove(pet);
                 uow.Save();
             }
